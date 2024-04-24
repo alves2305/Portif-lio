e587f80d033c49d38802cbe74054b5638d6382ac
alves2305 <raffa.alves.souza@gmail.com>
2024-04-24 17:43:22 -0300

    mediaquery

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function abrirArquivos() {
-    window.open('arquivos/Currículo - rafael.pdf')
-    window.open('arquivos/certificado.pdf')
+let cardVisible = false;
+
+// Oculta o cartão quando a página é carregada
+document.addEventListener("DOMContentLoaded", function() {
+    let card = document.querySelector(".card");
+    card.style.display = "none";
+});
+
+function clicar() {
+    let card = document.querySelector(".card");
+
+    if (cardVisible) {
+        card.style.display = "none";
+        cardVisible = false;
+    } else {
+        card.style.display = "block"; // ou "flex", dependendo do estilo do elemento
+        cardVisible = true;
+    }
 }
